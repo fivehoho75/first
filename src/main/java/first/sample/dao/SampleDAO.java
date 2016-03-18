8cea27f249ba7dc54f2d0e57fef59e0681ddf287
fivebird <fivebird@112.170.3.58>
2016-03-19 03:54:05 +0900

    게시판 초기단계

---
+++ src/main/java/first/sample/dao/SampleDAO.java
@@ -0,0 +1,17 @@
+package first.sample.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import first.common.dao.AbstractDAO;
+
+@Repository("sampleDAO")
+public class SampleDAO extends AbstractDAO{
+
+	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) {
+		return (List<Map<String, Object>>)selectList("sample.selectBoardList", map);
+	}
+
+}
